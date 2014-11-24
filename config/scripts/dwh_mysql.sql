a1895152970d957b2168241da04fb2c4fdb2d28a
Nelson Sousa <nsousa@gmail.com>
2014-11-24 08:34:43 +0000

    Changes to dim_customers and dim_product; Skeleton fact_orders ktr.

--- config/scripts/dwh_mysql.sql
+++ config/scripts/dwh_mysql.sql
@@ -3,60 +3,60 @@
 -- Tables required to run the sample tasks
 
 CREATE TABLE dim_date(
-	  date_id 					INT 			NOT NULL 	PRIMARY KEY
-	, year 						INT
-	, quarter					INT
-	, month_number 				INT
-	, month_name_full 			VARCHAR(10)
-	, month_name_short			VARCHAR(3)
-	, day_of_month				INT
-	, day_of_year				INT
-	, iso_year_of_week			INT
-	, iso_week_of_year			INT
-	, day_of_week 				INT
-	, day_of_week_full 			VARCHAR(10)
-	, day_of_week_short 		VARCHAR(3)
-	, date_string 				VARCHAR(10)
-	, date_value				DATE
-	, batch_id 					INT
-	) engine=myisam;
-	CREATE INDEX idx_dim_date_yqm ON dim_date( date_id, year, quarter, month_number, month_name_full );
-	CREATE INDEX idx_dim_date_yqmd ON dim_date( date_id, year, quarter, month_number, month_name_full, day_of_month, date_string );
-	CREATE INDEX idx_dim_date_yw ON dim_date( date_id, iso_year_of_week );
-	CREATE INDEX idx_dim_date_ywd ON dim_date( date_id, iso_year_of_week, day_of_week, day_of_week_full, date_string );
-	CREATE INDEX idx_dim_date_lookup ON dim_date( date_id, date_string );
+  date_id 					INT 			NOT NULL 	PRIMARY KEY
+, year 						INT
+, quarter					INT
+, month_number 				INT
+, month_name_full 			VARCHAR(10)
+, month_name_short			VARCHAR(3)
+, day_of_month				INT
+, day_of_year				INT
+, iso_year_of_week			INT
+, iso_week_of_year			INT
+, day_of_week 				INT
+, day_of_week_full 			VARCHAR(10)
+, day_of_week_short 		VARCHAR(3)
+, date_string 				VARCHAR(10)
+, date_value				DATE
+, batch_id 					INT
+) engine=myisam;
+CREATE INDEX idx_dim_date_yqm ON dim_date( date_id, year, quarter, month_number, month_name_full );
+CREATE INDEX idx_dim_date_yqmd ON dim_date( date_id, year, quarter, month_number, month_name_full, day_of_month, date_string );
+CREATE INDEX idx_dim_date_yw ON dim_date( date_id, iso_year_of_week );
+CREATE INDEX idx_dim_date_ywd ON dim_date( date_id, iso_year_of_week, day_of_week, day_of_week_full, date_string );
+CREATE INDEX idx_dim_date_lookup ON dim_date( date_id, date_string );
 
 
-	CREATE TABLE dim_time(
-	  time_id 					INT 			NOT NULL 	PRIMARY KEY
-	, hours24 					INT
-	, hours24_lpad 				VARCHAR(2)
-	, hours12 					INT
-	, hours12_lpad 				VARCHAR(2)
-	, am_pm_indicator			VARCHAR(2)
-	, minutes 					INT
-	, minutes_lpad 				VARCHAR(2)
-	, seconds 					INT
-	, seconds_lpad 				VARCHAR(2)
-	, time_formatted12_hm 		VARCHAR(7)
-	, time_formatted24_hm 		VARCHAR(5)
-	, time_formatted12_hms 		VARCHAR(11)
-	, time_formatted24_hms 		VARCHAR(9)
-	, batch_id 					INT
-	) engine=myisam;
-	CREATE INDEX idx_dim_time_hm12 ON dim_time( time_id, hours12, minutes, time_formatted12_hm, am_pm_indicator );
-	CREATE INDEX idx_dim_time_hm12lpad ON dim_time( time_id, hours12_lpad, minutes_lpad, time_formatted12_hm, am_pm_indicator );
-	CREATE INDEX idx_dim_time_hms12 ON dim_time( time_id, hours12, minutes, seconds, time_formatted12_hms, am_pm_indicator );
-	CREATE INDEX idx_dim_time_hms12lpad ON dim_time( time_id, hours12_lpad, minutes_lpad, time_formatted12_hms, am_pm_indicator );
-	CREATE INDEX idx_dim_time_hm24 ON dim_time( time_id, hours24, minutes, time_formatted24_hm );
-	CREATE INDEX idx_dim_time_hm24lpad ON dim_time( time_id, hours24_lpad, minutes_lpad, time_formatted24_hm );
-	CREATE INDEX idx_dim_time_hms24 ON dim_time( time_id, hours24, minutes, seconds, time_formatted24_hms );
-	CREATE INDEX idx_dim_time_hms24lpad ON dim_time( time_id, hours24_lpad, minutes_lpad, time_formatted24_hms );
-	CREATE INDEX idx_dim_time_lookup ON dim_time( time_id, time_formatted24_hms );
+CREATE TABLE dim_time(
+  time_id 					INT 			NOT NULL 	PRIMARY KEY
+, hours24 					INT
+, hours24_lpad 				VARCHAR(2)
+, hours12 					INT
+, hours12_lpad 				VARCHAR(2)
+, am_pm_indicator			VARCHAR(2)
+, minutes 					INT
+, minutes_lpad 				VARCHAR(2)
+, seconds 					INT
+, seconds_lpad 				VARCHAR(2)
+, time_formatted12_hm 		VARCHAR(7)
+, time_formatted24_hm 		VARCHAR(5)
+, time_formatted12_hms 		VARCHAR(11)
+, time_formatted24_hms 		VARCHAR(9)
+, batch_id 					INT
+) engine=myisam;
+CREATE INDEX idx_dim_time_hm12 ON dim_time( time_id, hours12, minutes, time_formatted12_hm, am_pm_indicator );
+CREATE INDEX idx_dim_time_hm12lpad ON dim_time( time_id, hours12_lpad, minutes_lpad, time_formatted12_hm, am_pm_indicator );
+CREATE INDEX idx_dim_time_hms12 ON dim_time( time_id, hours12, minutes, seconds, time_formatted12_hms, am_pm_indicator );
+CREATE INDEX idx_dim_time_hms12lpad ON dim_time( time_id, hours12_lpad, minutes_lpad, time_formatted12_hms, am_pm_indicator );
+CREATE INDEX idx_dim_time_hm24 ON dim_time( time_id, hours24, minutes, time_formatted24_hm );
+CREATE INDEX idx_dim_time_hm24lpad ON dim_time( time_id, hours24_lpad, minutes_lpad, time_formatted24_hm );
+CREATE INDEX idx_dim_time_hms24 ON dim_time( time_id, hours24, minutes, seconds, time_formatted24_hms );
+CREATE INDEX idx_dim_time_hms24lpad ON dim_time( time_id, hours24_lpad, minutes_lpad, time_formatted24_hms );
+CREATE INDEX idx_dim_time_lookup ON dim_time( time_id, time_formatted24_hms );
 
 
 
-CREATE TABLE dim_customers(
+CREATE TABLE dim_customer(
 	  customer_id				INTEGER			NOT NULL 	PRIMARY KEY 	AUTO_INCREMENT
 	, version					INTEGER
 	, date_from					DATETIME
@@ -70,7 +70,12 @@ CREATE TABLE dim_customers(
 	, stage_customer_batch_id	INTEGER			
 	, row_number				INTEGER
 	, batch_id					INTEGER
-);
+)engine=MyISAM;
+CREATE INDEX idx_dim_customer_batch_id on dim_customer(batch_id);
+CREATE INDEX idx_dim_customer_source_batch_id on dim_customer(source_batch_id);
+CREATE INDEX idx_dim_customer_lookup on dim_customer(customer_id, source_customer_key);
+CREATE INDEX idx_dim_customer_hierarchy on dim_customer(customer_id, country_name, territory, account_number);
+CREATE INDEX idx_dim_customer_type on dim_customer( customer_id, customer_type, account_number);
 
 
 CREATE TABLE dim_product(
@@ -78,6 +83,7 @@ CREATE TABLE dim_product(
 	, version					INTEGER
 	, date_from					DATETIME
 	, date_to					DATETIME
+	, source_product_key		INTEGER
 	, product					VARCHAR(450)
 	, category 					VARCHAR(150)
 	, subcategory				VARCHAR(150)
@@ -88,4 +94,20 @@ CREATE TABLE dim_product(
 	, stage_product_batch_id	INTEGER
 	, row_number				INTEGER
 	, batch_id					INTEGER
+)engine=MyISAM;
+CREATE INDEX idx_product_batch_id on dim_product(batch_id);
+CREATE INDEX idx_product_source_batch_id on dim_product(stage_product_batch_id);
+CREATE INDEX idx_product_lookup on dim_product(product_id, version, date_from, date_to, source_product_key);
+CREATE INDEX idx_product_hierarchy on dim_product(product_id, category, subcategory, product);
+
+
+CREATE TABLE dim_online_order(
+	online_order_id				INTEGER			NOT NULL 	PRIMARY KEY 	AUTO_INCREMENT
+,	online_order_type 			VARCHAR(1)
+, 	online_order_description 	VARCHAR(10)
 );
+INSERT INTO dim_online_order values( 1, null, null);
+INSERT INTO dim_online_order values( 2, 'Y', 'Online');
+INSERT INTO dim_online_order values( 3, 'N', 'In store');
+
+
